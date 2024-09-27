2a3796105aabee353eb659631e6a267432ff3169
jusluz <julioluz84@yahoo.com>
2024-09-27 01:34:57 -0300

    subindo projeto inicial Flask

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Jogoteca
+
+
+__Versões utilizadas:__
+Python: 3.9.9
+Flask: 3.0.0
+PyCharm: 2021.3.2
+Bootstrap: 5.1.x
+
+# How to
+
+pyenv install --list
+pyenv install 3.9.9
+pipx ensurepath
+poetry new jogoteca
+pyenv local 3.9.9
+poetry install
---
+++ jogoteca/jogoteca.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, render_template
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+        
+
+
+# Inicializador aplicação Flask
+app = Flask(__name__)
+
+# Define as rotas da aplicação.
+@app.route('/')
+
+def ola():
+    jogo1= Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('Pac-Man', 'Ação', 'Atari')
+    jogo3 = Jogo('Donkey Kong', 'Ação', 'Nintendo')
+
+
+    lista_de_jogos: list = list(jogo1, jogo2, jogo3)
+
+    return render_template('lista.html', titulo = 'Jogos', jogos = 'lista_de_jogos')
+
+
+@app.route('/novo')
+
+def novo():    
+    return render_template('novo.html', titulo = 'Novo Jogo')
+
+
+app.run()
+#Caso eu queira usara a porta 8080 para a aplicação ou permitir acessos internos
+# trecho da app
+# app.run(host='0.0.0.0', port=8080)
